5c58ef109e34508370131347385872abf14190ca
Mostafa Kazemi <m.kazemi.512@gmail.com>
2021-08-16 13:53:42 +0430

    Created Job and JobFactory

--- Shopping_Cart/Jobs/RemoveCartJob.cs
+++ Shopping_Cart/Jobs/RemoveCartJob.cs
@@ -3,10 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Quartz;
+using Shopping_Cart.Data;
 
 namespace Shopping_Cart.Jobs
 {
-    public class RemoveCartJob:IJob
+    [DisallowConcurrentExecution]
+    public class RemoveCartJob : IJob
     {
+        private MyContext _ctx;
+
+        public RemoveCartJob(MyContext context)
+        {
+            _ctx = context;
+        }
+        public Task Execute(IJobExecutionContext context)
+        {
+            var orders = _ctx.Orders.Where(o => o.IsFinally && o.CreateDate > DateTime.Now.AddHours(-24)).ToList();//orders that pass 24 hours
+            foreach (var order in orders)
+            {
+                var orderDetail = _ctx.OrderDetails.Where(od => od.OrderId == order.OrderId).ToList();
+                foreach (var detail in orderDetail)
+                {
+                    _ctx.Remove(detail);
+                }
+
+                _ctx.Remove(order);
+            }
+
+            _ctx.SaveChanges();
+            return Task.CompletedTask;
+        }
     }
 }
