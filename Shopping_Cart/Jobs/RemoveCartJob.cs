dfeb62fa42a60c860f51f44b14c056ea385e208b
Mostafa Kazemi <m.kazemi.512@gmail.com>
2021-08-16 13:38:00 +0430

    Added Quartz Package and Jobs Folder

---
+++ Shopping_Cart/Jobs/RemoveCartJob.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Quartz;
+
+namespace Shopping_Cart.Jobs
+{
+    public class RemoveCartJob:IJob
+    {
+    }
+}
